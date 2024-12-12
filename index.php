78c33c05ac588c2f65a7abf5176007279e992c47
Rizal Sinaga <showeko383@gmail.com>
2024-12-12 11:36:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ $categories = $conn->query("SELECT DISTINCT category FROM tasks ORDER BY categor
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To-Do List</title>
+    <title>To-Do-List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <style>
